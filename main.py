cfaf175e57eb3e47714a9a33174a7efbaa00fc27
t2kim <t2kim@uwaterloo.ca>
2023-03-01 12:31:59 -0500

    comment

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
 
 
+class Vector:
+	def __init__(self, x, y, z) -> None:
+		self.x = x
+		self.y = y
+		self.z = z
 
-print("hello")
+	def __add__(self, right):
+		return Vector(self.x + right.x, self.y + right.y, self.z + right.z)
 
+	def __str__(self) -> str:
+		return f"({self.x}, {self.y}, {self.z})"
 
+	def __mul__(self, right):
+		return Vector(0,0,0)
+
+myVector = Vector(1, 2, 3)
+
+secondVector = Vector(1, 2, 3)
+
+print(myVector + (secondVector))
