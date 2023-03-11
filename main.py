544ce968c113695ec9b79ecb044ef5abc8496c35
t2kim <t2kim@uwaterloo.ca>
2023-03-11 11:55:21 -0500

    comment

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 
+import math
 
 class Vector:
 	def __init__(self, x, y, z) -> None:
@@ -13,7 +14,13 @@ class Vector:
 		return f"({self.x}, {self.y}, {self.z})"
 
 	def __mul__(self, right):
-		return Vector(0,0,0)
+		return Vector(self.x*right.x, self.y*right.y, self.z*right.z)
+
+	def dist(self, right) -> float:
+		return sqrt((right.x-self.x)**2 + (right.y-self.y)**2 + (right.z-self.z)**2)
+
+	def crossProd(self, right):
+		return Vector(self.y*right.z - right.y*self.z, -(self.x*right.z - right.x*self.z), self.x*right.y-right.x*self.y)
 
 myVector = Vector(1, 2, 3)
 
