c2c0db7253d75d39c81733755a2f95de34671339
SKim <72927837+0610Thomas@users.noreply.github.com>
2023-03-11 13:06:07 -0500

    2 implement fma (#5)
    added dot multiplication, cross product, made class point, distance between points, force method

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
 
 
+
+import math
+
+
+
+class Point:
+	def __init__(self, x, y, z) -> None:
+                self.x = x
+                self.y = y
+                self.z = z
+
+	def dist(self, right) -> float:
+		'''
+		The space between two points. Return
+		'''
+		return sqrt((right.x-self.x)**2 + (right.y-self.y)**2 + (right.z-self.z)**2)
+
 class Vector:
 	def __init__(self, x, y, z) -> None:
 		self.x = x
@@ -13,10 +30,26 @@ class Vector:
 		return f"({self.x}, {self.y}, {self.z})"
 
 	def __mul__(self, right):
-		return Vector(0,0,0)
+		return Vector(self.x*right.x, self.y*right.y, self.z*right.z)
+
+	def crossProd(self, right):
+		return Vector(self.y*right.z - right.y*self.z, -(self.x*right.z - right.x*self.z), self.x*right.y-right.x*self.y)
+
+
+
+
+def force(mass: float, acc: Vector) -> Vector:
+	return Vector(mass* acc.x, mass* acc.y, mass* acc.z)
+
+
 
 myVector = Vector(1, 2, 3)
 
-secondVector = Vector(1, 2, 3)
+secondVector = Vector(2, 2, 2)
 
-print(myVector + (secondVector))
+mass = 10.0
+accel = Vector(2,3,1)
+
+print(force(mass, accel))
+
+
