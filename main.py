edde617ee9739608d34896f520b77501d7b0ccd2
t2kim <t2kim@uwaterloo.ca>
2023-03-11 12:59:00 -0500

    comment

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
 
+
+
 import math
+
+
+
+class Point:
+	def __init__(self, x, y, z) -> None:
+                self.x = x
+                self.y = y
+                self.z = z
+
+	def dist(self, right) -> float:
+		'''
+		The space between two points. Return
+		'''
+		return sqrt((right.x-self.x)**2 + (right.y-self.y)**2 + (right.z-self.z)**2)
 
 class Vector:
 	def __init__(self, x, y, z) -> None:
@@ -16,14 +32,24 @@ class Vector:
 	def __mul__(self, right):
 		return Vector(self.x*right.x, self.y*right.y, self.z*right.z)
 
-	def dist(self, right) -> float:
-		return sqrt((right.x-self.x)**2 + (right.y-self.y)**2 + (right.z-self.z)**2)
-
 	def crossProd(self, right):
 		return Vector(self.y*right.z - right.y*self.z, -(self.x*right.z - right.x*self.z), self.x*right.y-right.x*self.y)
 
+
+
+
+def force(mass: float, acc: Vector) -> Vector:
+	return Vector(mass* acc.x, mass* acc.y, mass* acc.z)
+
+
+
 myVector = Vector(1, 2, 3)
 
-secondVector = Vector(1, 2, 3)
+secondVector = Vector(2, 2, 2)
 
-print(myVector + (secondVector))
+mass = 10.0
+accel = Vector(2,3,1)
+
+print(force(mass, accel))
+
+
